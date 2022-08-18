cdf6a83cb24b66d38faa6d9b1e4404cbd4ae8318
Lee Si Yuan <lee.si.yuan.20@gmail.com>
2022-08-18 16:09:20 +0800

    updated answer

--- R2.js
+++ R2.js
@@ -8,22 +8,23 @@ It is giving rise to recursive process
 
 Q2
 A step refers to a single computation process that the programme runs.
+Steps refer to number of function applications
 
 
 Q3
-It takes 5 steps
+It takes 10 steps
 
 Q4
-It takes a total of 5 spaces (there are 5 deferred operations)
+It takes a total of 5 spaces (there are 4 deferred operations)
 
 Q5
 It takes 3 + 5 * 3 = 18 steps
 
 Q6
-It requires 0 space (there is no deferred operation)
+It requires constant space (there is no deferred operation)
 
 Q7
-It is giving rise to recursive process
+It is giving rise to recursive process (there is deferred operation)
 
 Q8
 It takes 3(from fib to fib_iter) + 5 * 3 (in fib_iter) = 18 steps to calculate fib(5)
