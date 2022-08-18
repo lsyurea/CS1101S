ee93544227cf2aed0af60827fd78027bf650f351
leesiyuan <No public email provided>
2022-08-18 11:18:34 +0800

    second reflection of CS1101S

---
+++ R2.js
@@ -0,0 +1,32 @@
+// Type your program in here!
+
+// R2
+
+/* 
+Q1
+It is giving rise to recursive process
+
+Q2
+A step refers to a single computation process that the programme runs.
+Evaluation done by the computer
+
+Q3
+It takes 5 steps
+
+Q4
+It takes a total of 5 spaces (there are 5 deferred operations)
+
+Q5
+It takes 2 * 5 = 15 steps
+
+Q6
+It requires only 1 space (there is only 1 deferred operation)
+
+Q7
+It is giving rise to recursive process
+
+Q8
+It takes 2 * 5 = 10 steps to calculate fib(5)
+
+Q9
+It requires only a space of 1 (there is only 1 deferred operation)
