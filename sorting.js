adb0ecf788dc122af5b413a2f85d1dc95227c8fa
leesiyuan <No public email provided>
2022-10-17 11:14:22 +0800

    Changes made from Source Academy

---
+++ sorting.js
@@ -0,0 +1,40 @@
+//in-place sorting
+
+function selection_sort(L) {
+    if (!is_null(L)) {
+        const len = length(L);
+        let cur_lst = tail(L);
+        while (!is_null(cur_lst)) {
+            if (head(L) > head(cur_lst)) {
+                const temp = head(L);
+                set_head(L, head(cur_lst));
+                set_head(cur_lst, temp);
+            }
+            cur_lst = tail(cur_lst);
+        }
+        selection_sort(tail(L));
+    }
+}
+
+
+
+function bubble_sort(L) {
+    function bubble(xs, end) {
+        if (length(xs) > end + 1) {
+            if (head(xs) > head(tail(xs))) {
+                const temp = head(xs);
+                set_head(xs, head(tail(xs)));
+                set_head(tail(xs), temp);
+            }
+            bubble(tail(xs), end);
+        }
+    }
+    
+    for (let i = 0; i < length(L) - 1; i = i + 1) {
+        bubble(L, i);
+    }
+}
+
+const LL = list(3, 5, 2, 4, 1, 3, 7, 9);
+bubble_sort(LL);
+LL;
